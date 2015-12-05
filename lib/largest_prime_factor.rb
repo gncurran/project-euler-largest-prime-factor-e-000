8857f4c2253dd4ffa49b70907463d25edf697d16
gncurran <gnc115@gmail.com>
2015-12-04 23:00:53 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,6 @@
-# Enter your procedural solution here!
+# What is the largest prime factor of the number 600851475143 ?
+require 'prime'
+def largest_prime_factor(n)
+  find_prime = Prime.prime_division(n)
+  find_prime.last[0]
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,13 @@
-# Enter your object-oriented solution here!
+# What is the largest prime factor of the number 600851475143 ?
+class LargestPrimeFactor
+  attr_accessor :n
+  
+  def initialize(n)
+    @n = n
+  end
+
+  def number
+    find_prime = Prime.prime_division(n)
+    find_prime.last[0]
+  end
+end
